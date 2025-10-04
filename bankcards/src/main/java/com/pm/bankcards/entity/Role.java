31bcf2b51617d625cc87d859e88825899c3f5f71
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-04 13:31:10 +0300

    adding Role entity

---
+++ bankcards/src/main/java/com/pm/bankcards/entity/Role.java
@@ -0,0 +1,26 @@
+package com.pm.bankcards.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 32)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
