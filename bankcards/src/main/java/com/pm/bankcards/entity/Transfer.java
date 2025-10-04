99a1f0e5fd8c1a320ca9df79da8c4c565301d334
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-04 13:50:14 +0300

    adding Transfer entity

---
+++ bankcards/src/main/java/com/pm/bankcards/entity/Transfer.java
@@ -0,0 +1,77 @@
+package com.pm.bankcards.entity;
+
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+@Table(name = "transfers")
+public class Transfer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "from_card_id")
+    private Card fromCard;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "to_card_id")
+    private Card toCard;
+
+    @Column(precision = 19, scale = 2, nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false, unique = true, length = 64)
+    private String requestId;
+
+    @Column(nullable = false)
+    private Instant createdAt = Instant.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public Card getFromCard() {
+        return fromCard;
+    }
+
+    public void setFromCard(Card fromCard) {
+        this.fromCard = fromCard;
+    }
+
+    public Card getToCard() {
+        return toCard;
+    }
+
+    public void setToCard(Card toCard) {
+        this.toCard = toCard;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+}
