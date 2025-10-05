94826f89d44f00d2f2212d0c0974f931da6df14a
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-05 17:00:17 +0300

    Auth user for Security context

---
+++ bankcards/src/main/java/com/pm/bankcards/security/AuthUser.java
@@ -0,0 +1,59 @@
+package com.pm.bankcards.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AuthUser implements UserDetails {
+
+    private final Long id;
+    private final String username;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+    private final boolean enabled;
+
+    public AuthUser(Long id, String username, String password,
+                    Collection<? extends GrantedAuthority> authorities, boolean enabled) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+        this.enabled = enabled;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+}
