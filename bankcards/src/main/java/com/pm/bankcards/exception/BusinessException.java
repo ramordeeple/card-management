b59adb0356aa0a667952c6416b558c6305ebf440
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-06 17:55:20 +0300

    refactored BusinessException.java

--- bankcards/src/main/java/com/pm/bankcards/exception/BusinessException.java
+++ bankcards/src/main/java/com/pm/bankcards/exception/BusinessException.java
@@ -1,25 +1,40 @@
 package com.pm.bankcards.exception;
 
+import org.springframework.http.HttpStatus;
+
 import java.util.Map;
 
-public class BusinessException extends RuntimeException {
-    private final String code;
-    private final Map<String, Object> details;
 
-    public BusinessException(String code, String message) {
-        this(code, message, Map.of());
+public class BusinessException extends RuntimeException {
+
+    private final ErrorCodes code;
+    private final Map<String, Object> details;
+    private final HttpStatus status;
+
+    public BusinessException(ErrorCodes code, String message) {
+        this(code, message, null, HttpStatus.BAD_REQUEST);
     }
 
-    public BusinessException(String code, String message, Map<String, Object> details) {
+    public BusinessException(ErrorCodes code, String message, Map<String, Object> details) {
+        this(code, message, details, HttpStatus.BAD_REQUEST);
+    }
+
+    public BusinessException(ErrorCodes code, String message, Map<String, Object> details, HttpStatus status) {
         super(message);
         this.code = code;
         this.details = details;
+        this.status = status;
     }
 
-    public String getCode() {
+    public ErrorCodes getCode() {
         return code;
     }
+
     public Map<String, Object> getDetails() {
         return details;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
     }
 }
