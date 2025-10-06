8bb0413c8be38e4aec8986b329dfceb29014836f
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-06 16:43:39 +0300

    JWT auth filter

---
+++ bankcards/src/main/java/com/pm/bankcards/security/JwtAuthFilter.java
@@ -0,0 +1,52 @@
+package com.pm.bankcards.security;
+
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtTokenService jwt;
+    private final CustomUserDetailsService users;
+
+    public JwtAuthFilter(JwtTokenService jwt, CustomUserDetailsService users) {
+        this.jwt = jwt;
+        this.users = users;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws ServletException, IOException {
+        String header = req.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            try {
+                String username = jwt.getUsername(token);
+                UserDetails user = users.loadUserByUsername(username);
+
+                var auth = new UsernamePasswordAuthenticationToken(
+                        user,
+                        null,
+                        user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+
+            } catch (JwtException e) {
+                res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token");
+                return;
+            }
+        }
+
+        chain.doFilter(req, res);
+    }
+}
