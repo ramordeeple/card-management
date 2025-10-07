4ff1027bc39d2e86c2a516f90b709b6746e6f1fc
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-07 10:04:56 +0300

    same card rule

---
+++ bankcards/src/main/java/com/pm/bankcards/service/rules/SameCardRule.java
@@ -0,0 +1,20 @@
+package com.pm.bankcards.service.rules;
+
+import com.pm.bankcards.exception.BusinessException;
+import com.pm.bankcards.exception.ErrorCodes;
+
+import java.util.Map;
+
+public class SameCardRule implements TransferRule {
+
+    @Override
+    public void check(TransferContext ctx) {
+        if (ctx.from().getId().equals(ctx.to().getId())) {
+            throw new BusinessException(
+                    ErrorCodes.SAME_CARD,
+                    "Transferring funds to the same card is not allowed",
+                    Map.of("cardId", ctx.from().getId())
+            );
+        }
+    }
+}
