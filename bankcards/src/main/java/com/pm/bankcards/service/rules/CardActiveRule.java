16b1e4b1d0316eb60e3c561ec9dc767f79671f46
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-07 10:44:05 +0300

    Card should be active rule

--- bankcards/src/main/java/com/pm/bankcards/service/rules/CardActiveRule.java
+++ bankcards/src/main/java/com/pm/bankcards/service/rules/CardActiveRule.java
@@ -1,4 +1,27 @@
 package com.pm.bankcards.service.rules;
 
-public class CardActiveRule {
+import com.pm.bankcards.dto.transfer.TransferSide;
+import com.pm.bankcards.entity.CardStatus;
+import com.pm.bankcards.exception.CardNotActiveException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardActiveRule implements TransferRule {
+
+    @Override
+    public void check(TransferContext ctx) {
+        if (ctx.from().getStatus() != CardStatus.ACTIVE)
+            throw new CardNotActiveException(
+                    ctx.from().getId(),
+                    TransferSide.FROM,
+                    ctx.from().getStatus().name()
+            );
+
+        if (ctx.to().getStatus() != CardStatus.ACTIVE)
+            throw new CardNotActiveException(
+                    ctx.to().getId(),
+                    TransferSide.TO,
+                    ctx.to().getStatus().name()
+            );
+    }
 }
