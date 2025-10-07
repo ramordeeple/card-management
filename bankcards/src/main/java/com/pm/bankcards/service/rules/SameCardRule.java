a099e55d63169869f2ed26bbec419912f0936622
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-07 10:06:09 +0300

    marking SameCardRule as component

--- bankcards/src/main/java/com/pm/bankcards/service/rules/SameCardRule.java
+++ bankcards/src/main/java/com/pm/bankcards/service/rules/SameCardRule.java
@@ -2,9 +2,11 @@ package com.pm.bankcards.service.rules;
 
 import com.pm.bankcards.exception.BusinessException;
 import com.pm.bankcards.exception.ErrorCodes;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@Component
 public class SameCardRule implements TransferRule {
 
     @Override
