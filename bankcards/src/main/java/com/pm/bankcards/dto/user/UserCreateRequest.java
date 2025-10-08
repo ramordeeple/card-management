561341d715e24227fd1545fb2d3ae3dc16f961cd
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-08 11:10:09 +0300

    adding openapi to dto user

--- bankcards/src/main/java/com/pm/bankcards/dto/user/UserCreateRequest.java
+++ bankcards/src/main/java/com/pm/bankcards/dto/user/UserCreateRequest.java
@@ -1,13 +1,17 @@
 package com.pm.bankcards.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "Создание пользователя (админ)")
 public record UserCreateRequest(
-        @NotBlank(message = "Name is required")
+        @Schema(example = "Bob", description = "Имя пользователя")
+        @NotBlank(message = "Требуется имя")
         String username,
 
+        @Schema(example = "qwerty123", description = "Пароль")
         @NotBlank
-        @Size(min = 8, message = "Password should contain at least 8 symbols")
+        @Size(min = 8, message = "Пароль должен содержать хотя бы 8 символов")
         String password
 ) {}
--- bankcards/src/main/java/com/pm/bankcards/dto/user/UserResponse.java
+++ bankcards/src/main/java/com/pm/bankcards/dto/user/UserResponse.java
@@ -1,10 +1,17 @@
 package com.pm.bankcards.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.Set;
 
+@Schema(description = "Информация о пользователе")
 public record UserResponse(
+        @Schema(example = "2", description = "ID пользователя")
         Long id,
+        @Schema(example = "Patrick", description = "Имя пользователя")
         String username,
+        @Schema(example = "[\"USER\"]", description = "Роли (админ, пользователь)")
         Set<String> roles,
+        @Schema(example = "true", description = "Активирован ли пользователь")
         boolean enabled
 ) {}
