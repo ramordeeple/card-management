3f6365aaf9464adf007c6a9e654740926cd16f7c
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-08 12:46:28 +0300

    making JwtTokenService working correctly

--- bankcards/src/main/java/com/pm/bankcards/security/JwtTokenService.java
+++ bankcards/src/main/java/com/pm/bankcards/security/JwtTokenService.java
@@ -5,13 +5,14 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.List;
 
-@Service
+@Component
 public class JwtTokenService {
 
     private final SecretKey key;
