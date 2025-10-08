74ff6615a77ef4b32feaad93ae1b816b69159cfd
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-08 12:41:39 +0300

    adding jparepository in RoleRepository.java

--- bankcards/src/main/java/com/pm/bankcards/repository/RoleRepository.java
+++ bankcards/src/main/java/com/pm/bankcards/repository/RoleRepository.java
@@ -1,9 +1,10 @@
 package com.pm.bankcards.repository;
 
 import com.pm.bankcards.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RoleRepository {
+public interface RoleRepository extends JpaRepository<Role,Long> {
     Optional<Role> findByName(String name);
 }
