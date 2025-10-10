323df1542cd71f68b4732de5adac35e581753667
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-10 19:07:29 +0300

    Test w/ db

---
+++ bankcards/src/test/java/com/pm/bankcards/it/ApplicationIT.java
@@ -0,0 +1,31 @@
+package com.pm.bankcards.it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public class ApplicationIT {
+
+    @Container
+    static final PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:latest");
+
+    @DynamicPropertySource
+    static void props(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.jpa.hibernate.dll-auto", () -> "validate");
+        registry.add("spring.liquidbase.enabled", () -> "true");
+    }
+
+    @Test
+    void contextLoads() {
+    }
+}
