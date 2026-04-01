555cea1d526e5cfb410e99f8838132ad8ed44536
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-04-01 04:59:46 +0300

    Enable caching in main

--- bankcards/src/main/java/com/pm/bankcards/BankCardsApplication.java
+++ bankcards/src/main/java/com/pm/bankcards/BankCardsApplication.java
@@ -3,9 +3,11 @@ package com.pm.bankcards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
+@EnableCaching
 public class BankCardsApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankCardsApplication.class, args);
