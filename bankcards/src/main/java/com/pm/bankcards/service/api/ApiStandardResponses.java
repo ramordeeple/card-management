71ea0efbf0053e93e0e7eb3b032f60413a8033bb
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-04-01 05:59:24 +0300

    Make ApiStandardResponses more neat

--- bankcards/src/main/java/com/pm/bankcards/service/api/ApiStandardResponses.java
+++ bankcards/src/main/java/com/pm/bankcards/service/api/ApiStandardResponses.java
@@ -11,10 +11,8 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @ApiResponses(value = {
-        @ApiResponse(responseCode = "204", description = "Request accepted"),
         @ApiResponse(responseCode = "401", description = "Unauthorized"),
         @ApiResponse(responseCode = "403", description = "No permission"),
-        @ApiResponse(responseCode = "404", description = "Card not found")
 })
 public @interface ApiStandardResponses {
 }
