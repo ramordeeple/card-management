d010b324d1ef3ad8ac4ceaabf346aed100c133a8
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-04-16 18:00:07 +0300

    Add methods for interface of user service

--- bankcards/src/main/java/com/pm/bankcards/service/api/UserService.java
+++ bankcards/src/main/java/com/pm/bankcards/service/api/UserService.java
@@ -2,6 +2,8 @@ package com.pm.bankcards.service.api;
 
 import com.pm.bankcards.dto.user.UserCreateRequest;
 import com.pm.bankcards.dto.user.UserResponse;
+import com.pm.bankcards.entity.User;
+import com.pm.bankcards.exception.NotFoundException;
 
 /**
  * Service for managing user profiles and registration.
@@ -11,7 +13,25 @@ public interface UserService {
     /**
      * Registers a new user in the system.
      * * @param req user registration data
+     *
      * @return {@link UserResponse} containing created user profile information
      */
     UserResponse create(UserCreateRequest req);
+
+    /**
+     * Получает полную сущность пользователя по его ID.
+     * Используется для передачи владельца в методы генерации карт.
+     *
+     * @param id идентификатор пользователя
+     * @return сущность {@link User}
+     * @throws NotFoundException если пользователь не найден
+     */
+    User getById(Long id);
+
+    /**
+     * Получает сущность текущего авторизованного пользователя из Security Context.
+     *
+     * @return сущность {@link User}
+     */
+    User getCurrentUser();
 }
