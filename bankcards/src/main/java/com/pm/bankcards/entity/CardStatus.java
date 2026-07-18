d3f980f3e920fc0ddcb30a527d3a57d0afc141c1
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-07-18 23:32:50 +0300

    refactor: Use english

--- bankcards/src/main/java/com/pm/bankcards/entity/CardStatus.java
+++ bankcards/src/main/java/com/pm/bankcards/entity/CardStatus.java
@@ -2,7 +2,7 @@ package com.pm.bankcards.entity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "Статус карты")
+@Schema(description = "Card status")
 public enum CardStatus {
     ACTIVE, BLOCKED, EXPIRED
 }
