e9d65a7f72e0deb42e41d8482c7d23c9f208a2f3
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-07-19 02:46:13 +0300

    fix: add mock of db seeder obj

--- bankcards/src/test/java/com/pm/bankcards/controller/CardControllerTest.java
+++ bankcards/src/test/java/com/pm/bankcards/controller/CardControllerTest.java
@@ -6,30 +6,23 @@ import com.pm.bankcards.security.JwtTokenService;
 import com.pm.bankcards.service.api.CardQueryService;
 import com.pm.bankcards.dto.card.CardResponse;
 import com.pm.bankcards.entity.CardStatus;
-import com.pm.bankcards.dto.card.CardFilter;
+import com.pm.bankcards.service.api.DatabaseSeeder;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
-import static org.springframework.security.config.http.MatcherType.mvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -38,11 +31,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc(addFilters = false)
 class CardControllerTest {
 
-    @Autowired MockMvc mvc;
-    @MockBean CardQueryService cardQueryService;
-    @MockBean JwtAuthFilter jwtAuthFilter;
-    @MockBean JwtTokenService jwtTokenService;
-    @MockBean CustomUserDetailsService customUserDetailsService;
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    CardQueryService cardQueryService;
+
+    @MockBean
+    JwtAuthFilter jwtAuthFilter;
+
+    @MockBean
+    JwtTokenService jwtTokenService;
+
+    @MockBean
+    CustomUserDetailsService customUserDetailsService;
+
+    @MockBean
+    DatabaseSeeder databaseSeeder;
 
     @Test
     void shouldReturnPagedCards() throws Exception {
@@ -55,13 +60,16 @@ class CardControllerTest {
                 CardStatus.ACTIVE,
                 BigDecimal.valueOf(15420.50)
         );
+
         var page = new PageImpl<>(List.of(card));
 
         when(cardQueryService.findMyCards(any(), any(), any())).thenReturn(page);
 
-        // создаём principal c полем id
+        // Create principal with id
         var principal = new UsernamePasswordAuthenticationToken(
-                new Object() { public Long id = 1L; },
+                new Object() {
+                    public Long id = 1L;
+                },
                 null,
                 List.of(new SimpleGrantedAuthority("ROLE_USER"))
         );
