9d9d92271d6525b92bcf63b73a0d308b6916a28c
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-07-19 07:17:42 +0300

    refactor: use RoleName enum instead as Set.of

--- bankcards/src/main/java/com/pm/bankcards/bootstrap/AdminBootstrap.java
+++ bankcards/src/main/java/com/pm/bankcards/bootstrap/AdminBootstrap.java
@@ -1,9 +1,7 @@
 package com.pm.bankcards.bootstrap;
 
-import com.pm.bankcards.entity.Role;
 import com.pm.bankcards.entity.User;
 import com.pm.bankcards.repository.UserRepository;
-import com.pm.bankcards.repository.RoleRepository;
 import com.pm.bankcards.security.RoleName;
 import org.springframework.beans.factory.annotation.Value;
 import lombok.extern.slf4j.Slf4j;
@@ -11,13 +9,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.util.Set;
+
 
 @Component
 @Slf4j
 public class AdminBootstrap implements CommandLineRunner {
     private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
     private final PasswordEncoder encoder;
 
     @Value("${admin.username}")
@@ -33,46 +30,37 @@ public class AdminBootstrap implements CommandLineRunner {
     String rawPassword;
 
     public AdminBootstrap(UserRepository userRepository,
-                          RoleRepository roleRepository,
                           PasswordEncoder encoder) {
         this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
         this.encoder = encoder;
     }
 
     @Override
     public void run(String... args) {
-        Role adminRole = roleRepository.findByName(RoleName.ADMIN)
-                .orElseGet(() -> roleRepository.save(Role.of(RoleName.ADMIN)));
+        userRepository.findByUsername(adminName)
+                .orElseGet(() -> {
+                    User admin = new User();
+                    admin.setUsername(adminName);
+                    admin.setPasswordHash(encoder.encode(adminPassword));
+                    admin.setRole(RoleName.ADMIN);
+                    admin.setEnabled(true);
 
-        Role userRole = roleRepository.findByName(RoleName.USER)
-                .orElseGet(() -> roleRepository.save(Role.of(RoleName.USER)));
+                    log.info("Admin was successfully created");
+                    return userRepository.save(admin);
+                });
 
 
-        userRepository.findByUsername(adminName).orElseGet(() -> {
-            User admin = new User();
-            admin.setUsername(adminName);
-            admin.setPasswordHash(encoder.encode(adminPassword));
-            admin.setEnabled(true);
-            admin.setRoles(Set.of(adminRole));
-            userRepository.save(admin);
+        userRepository.findByUsername(userName)
+                .orElseGet(() -> {
+                    User normal = new User();
+                    normal.setUsername(userName);
+                    normal.setPasswordHash(encoder.encode(rawPassword));
+                    normal.setEnabled(true);
+                    normal.setRole(RoleName.USER);
+                    userRepository.save(normal);
 
-            log.info("Admin ({} | {}) was successfully created", adminName, adminPassword);
-
-            return admin;
-        });
-
-        userRepository.findByUsername(userName).orElseGet(() -> {
-            User normal = new User();
-            normal.setUsername(userName);
-            normal.setPasswordHash(encoder.encode(rawPassword));
-            normal.setEnabled(true);
-            normal.setRoles(Set.of(userRole));
-            userRepository.save(normal);
-
-            log.info("A user for tests ({} | {}) was successfully created", userName, rawPassword);
-
-            return normal;
-        });
+                    log.info("A user for tests was successfully created");
+                    return normal;
+                });
     }
 }
